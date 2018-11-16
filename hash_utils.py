41a4ce9d84899d0e43b485119c859c7860a5c3f5
Daniyar Jakupov <ddr199305@gmail.com>
2018-11-16 10:30:42 +0100

    Convert block object to dict for hashing

--- hash_utils.py
+++ hash_utils.py
@@ -7,4 +7,6 @@ def hash_string_256(string):
 
 
 def hash_block(block):
-    return hash_string_256(json.dumps(block, sort_keys=True).encode())
+    # Convert block object to dict for hashing
+    hashable_block = block.__dict__.copy()
+    return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
