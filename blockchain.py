8624930530a89bc9adf57b29add3119ed53dd4fa
Daniyar Jakupov <ddr199305@gmail.com>
2018-11-17 09:38:30 +0100

    Use Transaction class  to create objects instead of dict

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,12 @@
 import functools
 import hashlib
-from collections import OrderedDict
 from hash_utils import hash_string_256, hash_block
 import json
 import os.path
 import pickle
 
 from block import Block
-
+from transaction import Transaction
 
 # Initializing blockchain list
 blockchain = []  # list of blocks
@@ -55,7 +54,8 @@ def save_data():
 
 
 def valid_proof(transactions, last_hash, proof):
-    guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+    guess = (str([tx.to_ordered_dict() for tx in transactions]) +
+             str(last_hash) + str(proof)).encode()
     guess_hash = hash_string_256(guess)
     return guess_hash[0:2] == '00'
 
@@ -72,16 +72,16 @@ def proof_of_work():
 def get_balance(participant):
     # list comprehension
     # get the list with amount of coins sent
-    tx_sender = [[tx['amount'] for tx in block.transactions if participant == tx['sender']]
+    tx_sender = [[tx.amount for tx in block.transactions if participant == tx.sender]
                  for block in blockchain]
-    open_tx_sender = [tx['amount']
-                      for tx in open_transactions if tx['sender'] == participant]
+    open_tx_sender = [tx.amount
+                      for tx in open_transactions if tx.sender == participant]
     tx_sender.append(open_tx_sender)
     # Calc total amount sent with reduce func
     amount_sent = functools.reduce(
         lambda tx_sum, tx_amount: tx_sum + sum(tx_amount) if len(tx_amount) > 0 else tx_sum + 0, tx_sender, 0)
     # get the list with amount of coins recieved
-    tx_recipient = [[tx['amount'] for tx in block.transactions if participant == tx['recipient']]
+    tx_recipient = [[tx.amount for tx in block.transactions if participant == tx.recipient]
                     for block in blockchain]
     # Calc total amount recieved with reduce func
     amount_recieved = functools.reduce(
@@ -110,39 +110,37 @@ def add_transaction(recipient, amount=1.0, sender=owner):
     #     "recipient": recipient,
     #     "amount": amount
     # }
-    transaction = OrderedDict([
-        ("sender", sender), ("recipient", recipient), ("amount", amount)
-    ])
+    transaction = Transaction(sender, recipient, amount)
+
     if verify_tx(transaction):
         open_transactions.append(transaction)
-        participants.add(sender)
-        participants.add(recipient)
         save_data()
         return True
     return False
 
 
 def verify_tx(transaction):
-    (_, _, sender_balance) = get_balance(transaction['sender'])
-    return sender_balance >= transaction['amount']
+    (_, _, sender_balance) = get_balance(transaction.sender)
+    return sender_balance >= transaction.amount
 
 
 def mine_block():
     last_block = blockchain[-1]  # get last_block from blockchain list
     # use list comprehention to loop over keys in last_block dict
-    # and create a new list w/ values from lsat_block dict
+    # and create a new list w/ values from last_block dict
     # then convert it to string
     hashed_block = hash_block(last_block)
     proof = proof_of_work()
-    reward_tx = OrderedDict([
-        ("sender", "MINING"), ("recipient", owner), ("amount", MINING_REWARD)
-    ])
+    reward_tx = Transaction('MINING', owner, MINING_REWARD)
+
     # Copy trans. instead of mutating original open_tx.
     copied_open_transactions = open_transactions[:]
     copied_open_transactions.append(reward_tx)
 
+    # Create a block object
     block = Block(len(blockchain),  hashed_block,
                   copied_open_transactions, proof)
+    # Add newly created block to blockchain
     blockchain.append(block)
     return True
 
@@ -191,7 +189,6 @@ while True:
     print('1: Add a new transaction')
     print('2: Mine new block')
     print('3: Show blockchain')
-    print('4: Show participants')
     print('5: Show balance')
     print('6: Show open transactions')
     print('7: Check transaction validity')
@@ -211,8 +208,6 @@ while True:
             save_data()
     elif user_choice == '3':
         print_blockchain_elements()
-    elif user_choice == '4':
-        print(participants)
     elif user_choice == '5':
         (amount_sent, amount_recieved, balance) = get_balance(owner)
         print(f'{owner} sent: {amount_sent:.2f}')
