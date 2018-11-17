15376cfb9df74f01f3817bb638f89247ba13e091
Daniyar Jakupov <ddr199305@gmail.com>
2018-11-17 09:28:45 +0100

    Convert tx objects to ordered_dict

--- hash_utils.py
+++ hash_utils.py
@@ -7,6 +7,9 @@ def hash_string_256(string):
 
 
 def hash_block(block):
-    # Convert block object to dict for hashing
+    # Convert block object to dictionary for json hashing
     hashable_block = block.__dict__.copy()
+    # Convert list of transactions from objects to ordered_dict for json hashing
+    hashable_block['transactions'] = [tx.to_ordered_dict()
+                                      for tx in hashable_block['transactions']]
     return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
