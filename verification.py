fc493260e51447b8af9b64fd80ae7442e43cfa0e
Daniyar Jakupov <ddr199305@gmail.com>
2018-11-21 08:18:19 +0100

    Add decorators to class methods

--- verification.py
+++ verification.py
@@ -2,27 +2,31 @@ from hash_utils import hash_string_256, hash_block
 
 
 class Verification:
-    def verify_chain(self, blockchain):
+    @classmethod
+    def verify_chain(cls, blockchain):
         """ Check if previous_hash value in block dict is equal to hashed version of previous block"""
         for (index, block) in enumerate(blockchain):
             if index == 0:
                 continue
             if block.previous_hash != hash_block(blockchain[index - 1]):
                 return False
-            if not self.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+            if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
                 print('Proof of work is invalid')
                 return False
         return True
 
-    def check_transaction_validity(self, open_transactions, get_balance):
+    @classmethod
+    def check_transaction_validity(cls, open_transactions, get_balance):
         ''' Check if all transactions are valid '''
-        return all([self.verify_tx(tx, get_balance) for tx in open_transactions])
+        return all([cls.verify_tx(tx, get_balance) for tx in open_transactions])
 
-    def verify_tx(self, transaction, get_balance):
+    @staticmethod
+    def verify_tx(transaction, get_balance):
         (_, _, sender_balance) = get_balance()
         return sender_balance >= transaction.amount
 
-    def valid_proof(self, transactions, last_hash, proof):
+    @staticmethod
+    def valid_proof(transactions, last_hash, proof):
         guess = (str([tx.to_ordered_dict() for tx in transactions]) +
                  str(last_hash) + str(proof)).encode()
         guess_hash = hash_string_256(guess)
