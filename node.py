0267e7fa82ea949670424f540e1aa214a6b062c8
Daniyar Jakupov <ddr199305@gmail.com>
2018-12-03 09:06:35 +0100

    Add simple flask server

--- node.py
+++ node.py
@@ -1,106 +1,16 @@
-from uuid import uuid4
-from blockchain import Blockchain
-from utils.verification import Verification
+from flask import Flask
+from flask_cors import CORS
 from wallet import Wallet
 
-
-class Node:
-    def __init__(self):
-        # self.wallet.public_key = str(uuid4())
-        self.wallet = Wallet()
-        self.wallet.create_keys()
-        self.blockchain = Blockchain(self.wallet.public_key)
-
-    def listen_for_input(self):
-        while True:
-            print('==================================')
-            print('Please choose')
-            print('0: Create wallet')
-            print('1: Load wallet')
-            print('2: Save wallet')
-            print('3: Add a new transaction')
-            print('4: Mine new block')
-            print('5: Show blockchain')
-            print('6: Show balance')
-            print('7: Show open transactions')
-            print('8: Check transaction validity')
-            print('q: Quit')
-            print('==================================')
-            user_choice = self.get_user_choice()
-            if user_choice == '0':  # Create wallet
-                self.wallet.create_keys()
-                self.blockchain = Blockchain(self.wallet.public_key)
-
-            elif user_choice == '1':  # Load wallet
-                self.wallet.load_keys()
-                self.blockchain = Blockchain(self.wallet.public_key)
-
-            elif user_choice == '2':  # Save wallet
-                self.wallet.save_keys()
-
-            elif user_choice == '3':  # Add new open transaction
-                tx_data = self.get_transaction_value()  # returns a tuple
-                recipient, amount = tx_data
-                signature = self.wallet.sign_transaction(
-                    self.wallet.public_key, recipient, amount)
-                if self.blockchain.add_transaction(recipient, self.wallet.public_key, signature, amount=amount):
-                    print('Added transaction!')
-                else:
-                    print('Transaction failed!')
-
-            elif user_choice == '4':  # Mine new block
-                if not self.blockchain.mine_block():
-                    print('Mining failed. Wallet wasnot found')
-
-            elif user_choice == '5':  # Show blockchain
-                self.print_blockchain_elements()
-
-            elif user_choice == '6':  # Show balance
-                (amount_sent, amount_recieved,
-                 balance) = self.blockchain.get_balance()
-                print(f'{self.wallet.public_key[:5]} sent: {amount_sent:.2f}')
-                print(
-                    f'{self.wallet.public_key[:5]} recieved: {amount_recieved:.2f}')
-                print(
-                    f'Balance of {self.wallet.public_key[:5]}: {balance:.2f}')
-
-            elif user_choice == '7':  # Show open transactions
-                print(self.blockchain.get_open_transactions())
-
-            elif user_choice == '8':
-                if Verification.check_transaction_validity(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
-                    print('All transactions are valid')
-                else:
-                    print('There are invalid transactions!')
-
-            elif user_choice == 'q':
-                break
-
-            else:
-                print('Input was invalid, please pick a value from the list!')
-
-            if not Verification.verify_chain(self.blockchain.chain):
-                print('Invalid blockchain')
-                break
-        print('Done!')
-
-    def get_transaction_value(self):
-        """ Returns a tuple w/ recipient and amount value """
-        tx_recipient = input('Enter the recipient: ')
-        tx_amount = float(input('Your transaction amount please: '))
-        return (tx_recipient, tx_amount)
-
-    def get_user_choice(self):
-        user_input = input('Your choice: ')
-        return user_input
-
-    def print_blockchain_elements(self):
-        # Output the blockchain list to the console
-        # for block in self.blockchain.chain:
-        #     print('Outputting Block')
-        #     print(block)
-        print(self.blockchain.chain)
+app = Flask(__name__)
+wallet = Wallet()
+CORS(app)
 
 
-node = Node()
-node.listen_for_input()
+@app.route('/', methods=['GET'])
+def get_ui():
+    return 'Hi there'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
